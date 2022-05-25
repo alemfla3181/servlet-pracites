80965cc8f50d8dd0b3b2278ca437960eda4bc32b
Dragon <alemfla3181@naver.com>
2022-05-25 11:45:17 +0900

    update guestbook#

--- sql/guestbook.sql
+++ sql/guestbook.sql
@@ -2,10 +2,12 @@
 
 desc guestbook;
 
-select * from guestbook;
+select * from guestbook order by no desc;
 
-select password from guestbook where no = 3;
+select password from guestbook where no > 0;
 
-insert into guestbook(no,name,password,message) values(null, "둘리", "1234", "gogogogogo");
+insert into guestbook(no,name,password,message,hostname) values(null, "둘리", "1234", "gogogogogo","ere");
+
+insert into guestbook(no,name,password,message,hostname) values(null, "또치", "425", "6364", "ㅇㅋㅇㅋ");
 
 delete from guestbook where no >50 and no < 77;
